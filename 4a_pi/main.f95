a936d8d9355fd562d122077aa1c6061bfc422c31
Dr. Suess <heutekeinnachtisch@gmail.com>
2017-05-06 17:22:38 +0200

    Update main.f95

--- 4a_pi/main.f95
+++ 4a_pi/main.f95
@@ -1,48 +1,40 @@
+
 program main
-	use mod_calc
-	use mpi
-	
-	implicit none
-	!
-	integer(kind=4), parameter :: numberOfSegments = int(1e9) ! Anzahl der Stützstellen für das Integral
-	double precision, parameter :: 	lowerBoundary = 0 ! Untergrenze für das Integral
-	double precision, parameter :: 	upperBoundary = 1 ! Obergrenze für das Integral
-	!
-	double precision :: temp ! Zwischenspeicher für Ergebnis
-	integer :: i ! Schleifenvariable
-	integer :: ierr ! Fehlercode für MPI
-	
-	integer(kind=4) :: numbers = int(1e9) ! Anzahl der Stützstellen für das Integral
+        use mpi
+        use mod_calc
+        !
+        implicit none
+        ! for MPI
+        integer :: ierr ! Fehlercode
+        integer :: rank, status(MPI_STATUS_SIZE)
+        !
+        integer, parameter :: cpus = 4 !number of used cpus
+        integer(kind=4), parameter :: numberOfSegments = int(1e9) ! Anzahl der Stützstellen für das Integral
+        double precision, parameter :: lowerBoundary = 0 ! Untergrenze für das Integral
+        double precision, parameter :: upperBoundary = 1 ! Obergrenze für das Integral
+        double precision :: temp, summe ! Zwischenspeicher für Ergebnis
+        integer :: i !Schleifencounter
+        double precision :: stepSize ! Größe der Abschnitte die einzeln gerechnet werden
+        stepSize = (upperBoundary - lowerBoundary)/cpus
 
-	call MPI_INIT(ierr) 
 
-	numbers = 5
-    call integral(lowerBoundary, upperBoundary, numbers, temp)
-	print *, "precision: ", numbers, "pi: ", temp
-	numbers = 10
-    call integral(lowerBoundary, upperBoundary, numbers, temp)
-	print *, "precision: ", numbers, "pi: ", temp
-	numbers = 20
-    call integral(lowerBoundary, upperBoundary, numbers, temp)
-	print *, "precision: ", numbers, "pi: ", temp
-	numbers = 1000
-    call integral(lowerBoundary, upperBoundary, numbers, temp)
-	print *, "precision: ", numbers, "pi: ", temp
-	numbers = 1000000
-    call integral(lowerBoundary, upperBoundary, numbers, temp)
-	print *, "precision: ", numbers, "pi: ", temp
-	numbers = 10000000
-    call integral(lowerBoundary, upperBoundary, numbers, temp)
-	print *, "precision: ", numbers, "pi: ", temp
-	
-	
-	call MPI_FINALIZE(ierr)        
-	
-	
-!	print *, "von google:", 0, "pi: ", "   3.14159265359"
-	print *, "von pibel: ", 0, "pi: ", "   3.141592653589793238"
-	
-	
-	print *, "ENDE"
+        call mpi_init(ierr)
+        call mpi_comm_rank(mpi_comm_world, rank, ierr)
+
+        call integral((rank * stepSize), ((rank + 1) * stepSize), (numberOfSegments/cpus), temp)
+
+        select case (rank)
+        case (0) !master
+                summe = temp
+                do i = 1, (cpus -1)
+                        call mpi_recv(temp, 1, mpi_double, i, 2017, mpi_comm_world, status, ierr)
+                        summe = summe + temp
+                end do
+                print *, "pi: ", summe
+        case default
+                call mpi_send(temp, 1, mpi_double, 0, 2017, mpi_comm_world, ierr)
+        end select
+
+        call mpi_finalize(ierr)
 
 end program
